5c2781ac1603661c69c8fe69638a315929967b00
alex123566545 <139175706+alex123566545@users.noreply.github.com>
2026-05-14 16:38:36 -0500

    ,

--- app.py
+++ app.py
@@ -1,33 +1,117 @@
 import streamlit as st
+import pandas as pd
+
+# =====================================
+# CONFIG
+# =====================================
 
 st.set_page_config(
     page_title="Sistema Predictivo",
-    page_icon="📊",
-    layout="wide",
-    initial_sidebar_state="expanded"
+    layout="wide"
 )
 
 # =====================================
-# FORZAR VISIBILIDAD DEL SIDEBAR
+# MENU LATERAL
 # =====================================
 
-st.markdown("""
-<style>
-
-[data-testid="stSidebar"] {
-    min-width: 250px;
-    max-width: 250px;
-}
-
-</style>
-""", unsafe_allow_html=True)
+pagina = st.sidebar.radio(
+    "📂 Navegación",
+    [
+        "Inicio",
+        "Dashboard",
+        "Predicción",
+        "Historial"
+    ]
+)
 
 # =====================================
-# MAIN
+# INICIO
 # =====================================
 
-st.title("📈 Sistema Predictivo de Ventas")
+if pagina == "Inicio":
 
-st.write("Bienvenido al sistema predictivo.")
+    st.title("📈 Sistema Predictivo de Ventas")
 
-st.info("⬅️ Usa el menú lateral izquierdo para navegar.")
+    st.write("""
+    Bienvenido al sistema predictivo.
+    """)
+
+# =====================================
+# DASHBOARD
+# =====================================
+
+elif pagina == "Dashboard":
+
+    st.title("📊 Dashboard")
+
+    st.metric(
+        "Ventas Totales",
+        "S/ 25,000"
+    )
+
+    st.metric(
+        "Predicciones",
+        "350"
+    )
+
+# =====================================
+# PREDICCION
+# =====================================
+
+elif pagina == "Predicción":
+
+    st.title("🤖 Predicción de Ventas")
+
+    fecha = st.date_input("Fecha")
+
+    hora = st.number_input(
+        "Hora",
+        min_value=0,
+        max_value=23
+    )
+
+    producto = st.selectbox(
+        "Producto",
+        [
+            "Coca Cola",
+            "Red Bull",
+            "Inca Kola"
+        ]
+    )
+
+    precio = st.number_input(
+        "Precio",
+        min_value=0.0
+    )
+
+    clima = st.selectbox(
+        "Clima",
+        [
+            "Soleado",
+            "Lluvioso",
+            "Nublado"
+        ]
+    )
+
+    if st.button("Predecir"):
+
+        prediccion_fake = 24
+
+        st.success(
+            f"Cantidad vendida estimada: {prediccion_fake}"
+        )
+
+# =====================================
+# HISTORIAL
+# =====================================
+
+elif pagina == "Historial":
+
+    st.title("📜 Historial")
+
+    df = pd.DataFrame({
+        "Producto": ["Coca Cola", "Red Bull"],
+        "Predicción": [20, 35]
+    })
+
+    st.dataframe(df)
