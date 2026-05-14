4135823379fd04e730c6c037f36fadbe6b26d337
alex123566545 <139175706+alex123566545@users.noreply.github.com>
2026-05-14 16:28:52 -0500

    .

--- app.py
+++ app.py
@@ -14,3 +14,12 @@ Bienvenido al sistema predictivo.
 
 Usa el menú lateral para navegar.
 """)
+st.markdown("""
+<style>
+
+section[data-testid="stSidebar"] {
+    background-color: #1E1E1E;
+}
+
+</style>
+""", unsafe_allow_html=True)
