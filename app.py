afa1850ff3e988103c8188118936383b0da60860
alex123566545 <139175706+alex123566545@users.noreply.github.com>
2026-05-19 08:26:44 -0500

    .

--- app.py
+++ app.py
@@ -23,30 +23,3 @@ pagina = st.sidebar.radio(
     ]
 )
 
-# =====================================
-# PAGINAS
-# =====================================
-
-if pagina == "Inicio":
-
-    st.title("📈 Sistema Predictivo")
-
-    st.write("Bienvenido al sistema.")
-
-elif pagina == "Dashboard":
-
-    from pages.dashboard import show_dashboard
-
-    show_dashboard()
-
-elif pagina == "Predicción":
-
-    from pages.prediccion import show_prediccion
-
-    show_prediccion()
-
-elif pagina == "Historial":
-
-    from pages.historial import show_historial
-
-    show_historial()
