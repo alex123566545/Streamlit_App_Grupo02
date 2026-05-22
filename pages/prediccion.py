a063bcba4fd899d49205214c84bf916c356e3335
alex123566545 <139175706+alex123566545@users.noreply.github.com>
2026-05-21 20:09:45 -0500

    .

--- pages/prediccion.py
+++ pages/prediccion.py
@@ -54,7 +54,7 @@ def _load_pkl(url: str):
 @st.cache_resource(show_spinner="Cargando modelo predictivo…")
 def load_assets():
     MODEL_URL    = "https://klbmaoqxfvjsczwrrwkj.supabase.co/storage/v1/object/public/models/model.pl"
-    FEATURES_URL = "https://klbmaoqxfvjsczwrrwkj.supabase.co/storage/v1/object/public/models/features.pkl"
+    FEATURES_URL = "https://klbmaoqxfvjsczwrrwkj.supabase.co/storage/v1/object/public/models/features.pl"
     return _load_pkl(MODEL_URL), _load_pkl(FEATURES_URL)
 
 
